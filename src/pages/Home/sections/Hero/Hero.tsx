bc6b37de8391f49f0186c185cfbb54dc75c80ab8
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-04-21 13:11:37 -0300

    add image

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,5 +1,29 @@
+import { Grid, styled } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.jpg";
+
 const Hero = () => {
-  return <>Hero</>;
+  const StyleHero = styled("div")(() => ({
+    backgroundColor: "black",
+    color: "white",
+  }));
+
+  const StyleImg = styled("img")(() => ({
+    width: "30%",
+    borderRadius: "50%",
+  }));
+
+  return (
+    <>
+      <StyleHero>
+        <Grid container spacing={2}>
+          <Grid size={4}>
+            <StyleImg src={Avatar} />
+          </Grid>
+          <Grid size={8}>size=4</Grid>
+        </Grid>
+      </StyleHero>
+    </>
+  );
 };
 
 export default Hero;
